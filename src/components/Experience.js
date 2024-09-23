8e3015aa611b3e557c1915ddd0ae663ed6a5e5e4
Aleksandar <aleksandar.vukojevicc@gmail.com>
2024-09-23 12:01:53 +0200

    model added

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,3 +1,27 @@
 export default function Experience() {
+
+    // Configurator options
+    var options = { 
+        distID: 'latest', 
+        solution3DName: 'suitcase-color', 
+        projectName: 'resources-for-videos-and-marketing-purposes', 
+        solution3DID: '62766', containerID: 'container3d_replace'
+    
+        // onPointerClick: function (objectsClick) {
+        //     if (objectsClick.length > 0) {
+        //         if (objectsClick[0].type == "annotation") {
+        //             // 
+        //         }
+        //     }
+        // },
+    
+        // onCameraInteraction: function () {
+        //     //       
+        // },
+    };
+    
+    Unlimited3D.init(options);
+    // Configurator options
+
     return null
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import Experience from './components/Experience'
 import './styles/main.scss'
 import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector('#container3d_replace'))
 
 root.render(<Experience/>)
