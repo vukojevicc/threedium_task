c4a720a60a6d4c12a50a0a3e779ccce963dc6889
Aleksandar <aleksandar.vukojevicc@gmail.com>
2024-09-23 09:50:21 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Experience from './components/Experience'
+import './styles/main.scss'
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(<Experience/>)
